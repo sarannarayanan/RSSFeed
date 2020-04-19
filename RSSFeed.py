a8388f3dbfd6aeb05205642cb537e111e1ce1ba8
sarannarayanan <saranyann@wilcosource.com>
2020-04-19 16:41:07 +0530

    Update RSSFeed.py

--- RSSFeed.py
+++ RSSFeed.py
@@ -40,6 +40,37 @@ def rss():
 
     return feed.rss()
 
+@app.route("/crmrss", methods=["GET"])
+
+def crmrss():
+    item1 = Item(
+        title = "Salesforce Sales Cloud",
+        link = "https://www.Salesforce.com", 
+        description = "Salesforce Sales Cloud in Lightning",
+        author = "Saranyan Narayanan",
+        guid = Guid("https://www.Salesforce.com"),
+        pubDate = datetime.datetime(2020, 4, 16, 14, 15)
+    
+    )
+
+    item2 = Item(
+        title = "LWC Hub",
+        link = "https://www.lwchub.com/techtalk/lwc-common", 
+        description = "LWC Hub is a great resource for LWC",
+        author = "Saranyan Narayanan",
+        guid = Guid("https://www.lwchub.com/"),
+        pubDate = datetime.datetime(2020, 4, 16, 14, 15))
+
+    feed = Feed(
+        title = "Sample RSS Feed",
+        link = "https://herokuhero-sharan.herokuapp.com/rss",
+        description = "This is an example of how to use rfeed to generate an RSS 2.0 feed",
+        language = "en-IN",
+        lastBuildDate = datetime.datetime.now(),
+        items = [item1])
+
+    return feed.rss()
+
 if __name__ == "__main__":
     # This allows us to use a plain HTTP callback
     #os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = "1"
