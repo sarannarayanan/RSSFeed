829e3f07a9ea3d29dc3391d0a3d4b11a1ccfef22
sarannarayanan <saranyann@wilcosource.com>
2020-04-21 21:35:10 +0530

    Update RSSFeed.py

--- RSSFeed.py
+++ RSSFeed.py
@@ -13,6 +13,17 @@ from urllib.parse import parse_qs
 def hello():
     return redirect("/rss", code=302)
 
+@app.route('/json')
+    abcd = {
+        "uid": "urn:uuid:1335c695-cfb8-4ebb-abbd-80da344efa6b",
+        "updateDate": "2016-05-23T22:34:51.0Z",
+        "titleText": "Amazon Developer Blog, week in review May 23rd",
+        "mainText": "",
+        "streamUrl": "https://developer.amazon.com/public/community/blog/myaudiofile.mp3",
+        "redirectionUrl": "https://developer.amazon.com/public/community/blog"
+        }
+    return json.dumps(abcd, sort_keys=True, indent=2, separators=(',', ': '))
+
 @app.route("/rss", methods=["GET"])
 
 def rss():
