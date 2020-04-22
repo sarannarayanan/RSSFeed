790e22117c5e960037aefba1524f4c7d332e3c62
sarannarayanan <saranyann@wilcosource.com>
2020-04-22 15:53:21 +0530

    Update RSSFeed.py

--- RSSFeed.py
+++ RSSFeed.py
@@ -17,7 +17,7 @@ def hello():
 def jsonfeed():
     abcd = {
         "uid": "urn:uuid:1335c695-cfb8-4ebb-abbd-80da344efa6b",
-        "updateDate": "2016-05-23T22:34:51.0Z",
+        "updateDate": "2020-03-23T22:34:51.0Z",
         "titleText": "Amazon Developer Blog, week in review May 23rd",
         "mainText": "",
         "streamUrl": "https://developer.amazon.com/public/community/blog/myaudiofile.mp3",
